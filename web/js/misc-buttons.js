9b2ca4866bd96fe8a61a1abda0d631a8e0282b77
Cybernetic1 <generic.intelligence@gmail.com>
2024-09-25 14:17:16 +0800

    rotate Chinese and invert English

--- web/js/misc-buttons.js
+++ web/js/misc-buttons.js
@@ -14,7 +14,10 @@ white_box.onkeypress = function(e) {
 
 	// console.log("Ctrl=", e.ctrlKey);
 	// console.log("keyCode=", keyCode);
-	if (keyCode === 13 || keyCode === 10) {		// enter key = 13 or 10
+
+	if (keyCode === 13) {		// enter key = 13 or 10
+		if (e.shiftKey)			// if shift, process as normal enter
+			return true;
 		// Don't know why but enter = 13, ctrl + enter = 10
 		if (dreamState.value == "all")
 			// normally, ctrlKey causes to send to Dream
@@ -318,100 +321,210 @@ document.getElementById("▼").addEventListener("click", function() {
 });
 
 document.getElementById("do-log").addEventListener("click", function() {
-		// **** No need to get date & time -- server will do that automatically
-		// Get date and time
-		// var date = new Date();
-		// var logName = date.toDateString().replace(/ /g,"-");
-		const fname = document.getElementById("white-box").value;
-		if (fname.length == 0)
-			send2Chat("!log quick");
-		else
-			send2Chat("!log " + fname);
-		var audio = new Audio("sending.ogg");
-		audio.play();
+	// **** No need to get date & time -- server will do that automatically
+	// Get date and time
+	// var date = new Date();
+	// var logName = date.toDateString().replace(/ /g,"-");
+	const fname = document.getElementById("white-box").value;
+	if (fname.length == 0)
+		send2Chat("!log quick");
+	else
+		send2Chat("!log " + fname);
+	var audio = new Audio("sending.ogg");
+	audio.play();
 }, false);
 
 document.getElementById("do-history").addEventListener("click", function() {
-		send2Chat("!his");
-		var audio = new Audio("sending.ogg");
-		audio.play();
+	send2Chat("!his");
+	var audio = new Audio("sending.ogg");
+	audio.play();
 }, false);
 
 document.getElementById("do-shell").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		send2Chat("!sh " + str);
+	str = document.getElementById("white-box").value;
+	send2Chat("!sh " + str);
 }, false);
 
 document.getElementById("do-URL-unescape").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		str2 = decodeURIComponent(str);
-		document.getElementById("white-box").value = str2;
+	str = document.getElementById("white-box").value;
+	str2 = decodeURIComponent(str);
+	document.getElementById("white-box").value = str2;
 }, false);
 
 document.getElementById("do-URL-escape").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		str2 = encodeURIComponent(str);
-		document.getElementById("white-box").value = str2;
+	str = document.getElementById("white-box").value;
+	str2 = encodeURIComponent(str);
+	document.getElementById("white-box").value = str2;
 }, false);
 
 document.getElementById("do-traditionalize").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		str2 = traditionalize(str);
-		document.getElementById("white-box").value = str2;
+	str = document.getElementById("white-box").value;
+	str2 = traditionalize(str);
+	document.getElementById("white-box").value = str2;
 }, false);
 
 document.getElementById("do-Google").addEventListener("click", function() {
-		// Open browser and search Google
-		str = document.getElementById("white-box").value;
-		window.open("https://www.google.com/search?q=" + str);
+	// Open browser and search Google
+	str = document.getElementById("white-box").value;
+	window.open("https://www.google.com/search?q=" + str);
 }, false);
 
 document.getElementById("do-Mandarin").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		// Copy to Red Box
-		//document.getElementById("pinyin-box").value = str;
-		$.ajax({
-			method: "POST",
-			url: "./speakMandarin",
-			contentType: "application/json; charset=utf-8",
-			processData: false,
-			data: str,
-			success: function(resp) {
-				console.log("Mandarin: " + str);
-			}
-		});
+	str = document.getElementById("white-box").value;
+	// Copy to Red Box
+	//document.getElementById("pinyin-box").value = str;
+	$.ajax({
+		method: "POST",
+		url: "./speakMandarin",
+		contentType: "application/json; charset=utf-8",
+		processData: false,
+		data: str,
+		success: function(resp) {
+			console.log("Mandarin: " + str);
+		}
+	});
 }, false);
 
 document.getElementById("do-Cantonese").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		// Copy to Pink Box
-		// document.getElementById("pink-box").value = str;
-		// cantonize(str);
-		// Pronunciate it
-		$.ajax({
-			method: "POST",
-			url: "/speakCantonese",
-			contentType: "application/json; charset=utf-8",
-			processData: false,
-			data: str,
-			success: function(resp) {
-				console.log("Mandarin: " + str);
-				}
-		});
+	str = document.getElementById("white-box").value;
+	// Copy to Pink Box
+	// document.getElementById("pink-box").value = str;
+	// cantonize(str);
+	// Pronunciate it
+	$.ajax({
+		method: "POST",
+		url: "/speakCantonese",
+		contentType: "application/json; charset=utf-8",
+		processData: false,
+		data: str,
+		success: function(resp) {
+			console.log("Mandarin: " + str);
+			}
+	});
 }, false);
 
 document.getElementById("do-pinyin").addEventListener("click", function() {
-		str = document.getElementById("white-box").value;
-		display_pinyin(str);
-		// Pronunciate it
-		$.ajax({
-			method: "POST",
-			url: "/speakMandarin/",
-			data: str,
-			success: function(resp) {
-				// nothing
-				}
+	str = document.getElementById("white-box").value;
+	display_pinyin(str);
+	// Pronunciate it
+	$.ajax({
+		method: "POST",
+		url: "/speakMandarin/",
+		data: str,
+		success: function(resp) {
+			// nothing
+			}
+	});
+}, false);
+
+document.getElementById("do-rotate").addEventListener("click", function() {
+	str = document.getElementById("white-box").value;
+	// break str into lines
+	strs = str.split('\n');
+	const numlines = strs.length;
+	console.log(numlines);
+	const maxlen = strs.reduce((m,x) => x.length > m ? x.length : m, 0);
+	// for each line, pad to max length
+	const strs2 = strs.map(function(line) {
+		return line.padEnd(maxlen, '　');	// note: this is a double-width space
 		});
+	console.log(strs2);
+	var strs3 = [];
+	for (var j = 0; j < maxlen; ++j)
+		strs3.push("");
+	console.log(strs3);
+	//console.log(strs2[1][1]);
+	for (var i = 0; i < numlines; ++i)
+		for (var j = 0; j < maxlen; ++j)
+			strs3[j] += strs2[i][j];
+
+	document.getElementById("white-box").value = strs3.join('\n');
+	console.log(strs3);
+}, false);
+
+var flipTable = {
+'\u0021' : '\u00A1',
+'\u0022' : '\u201E',
+'\u0026' : '\u214B',
+'\u0027' : '\u002C',	// '
+'\u0028' : '\u0029',
+'\u002E' : '\u02D9',
+'\u0031' : '\uA781',	// 1
+'\u0032' : '\u2D52',	// 2
+'\u0033' : '\u0190',
+'\u0034' : '\u07C8',	// 4
+'\u0036' : '\u0039',
+'\u0037' : '\u2C62',
+'\u003B' : '\u061B',
+'\u003C' : '\u003E',
+'\u003F' : '\u00BF',
+'\u0041' : '\u2200',
+'\u0042' : '\uA79A',	// B
+'\u0043' : '\u2183',
+'\u0044' : '\uA4F7',	// D 2ACF
+'\u0045' : '\u018E',
+'\u0046' : '\u2132',
+'\u0047' : '\u2141',
+'\u004A' : '\u017F',
+'\u004B' : '\uA7B0',	// K
+'\u004C' : '\u2142',
+'\u004D' : '\u0057',
+'\u004E' : '\u1D0E',
+'\u0050' : '\u0500',
+'\u0051' : '\uA779',	// Q
+'\u0052' : '\u1D1A',
+'\u0054' : '\uA7B1',	// T
+'\u0055' : '\u2229',
+'\u0056' : '\u1D27',
+'\u0059' : '\u2144',
+'\u005B' : '\u005D',
+'\u005F' : '\u203E',
+'\u0061' : '\u0250',
+'\u0062' : '\u0071',
+'\u0063' : '\u0254',
+'\u0064' : '\u0070',
+'\u0065' : '\u01DD',
+'\u0066' : '\u2A0D',	// f
+'\u0067' : '\u0183',
+'\u0068' : '\u0265',
+'\u0069' : '\u1D09',	// i
+'\u006A' : '\u1E5B',	// j 1E5B
+'\u006B' : '\u029E',
+'\u006C' : '\uA781',	// l
+'\u006D' : '\u026F',
+'\u006E' : '\u0075',
+'\u0072' : '\u0279',
+'\u0074' : '\u0287',	// t
+'\u0076' : '\u028C',
+'\u0077' : '\u028D',
+'\u0079' : '\u028E',
+'\u007B' : '\u007D',
+'\u203F' : '\u2040',
+'\u2045' : '\u2046',
+'\u2234' : '\u2235'
+}
+
+for (i in flipTable)
+	flipTable[flipTable[i]] = i;
+
+document.getElementById("do-flip").addEventListener("click", function() {
+	str = document.getElementById("white-box").value;
+	// break str into lines
+	strs = str.split('\n');
+	const numlines = strs.length;
+	console.log(numlines);
+	const strs2 = strs.map(function(line) {
+		const last = line.length - 1;
+		result = new Array(line.length);
+		for (var i = last; i >= 0; --i) {
+			var c = line.charAt(i);
+			var r = flipTable[c];
+			result[last - i] = r != undefined ? r : c;
+			}
+		return result.join('');
+		});
+	console.log(strs2);
+	document.getElementById("white-box").value = strs2.reverse().join('\n');
 }, false);
 
 document.getElementById("do-resize").addEventListener("click", function() {
