466a899ffa316b430fa48d2c0923f58df55937ba
Cybernetic1 <generic.intelligence@gmail.com>
2024-11-19 20:44:54 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -18,14 +18,14 @@ Conkey_PID=$!
 echo $Conkey_PID > conkey_PID.txt
 sleep 1.5
 ##### start chrome browser
-if chromium-browser --version >/dev/null; then
-	chromium-browser --app=http://localhost:8484/Cantonese-input.html &
-else
-    # google-chrome --new-window http://localhost:8484/index.html
-	# vivaldi --app=http://localhost:8484/index.html
-    browser --app=http://localhost:8484/Cantonese-input.html &
-fi
-sleep 4
+# if chromium-browser --version >/dev/null; then
+chromium-browser --app=http://localhost:8484/Cantonese-input.html
+# else
+# google-chrome --new-window http://localhost:8484/index.html
+# vivaldi --app=http://localhost:8484/index.html
+# browser --app=http://localhost:8484/Cantonese-input.html
+# fi
+sleep 15
 ##### set size and flags of Conkey broswer window
 wmctrl -r "iCant" -b remove,maximized_horz,maximized_vert
 wmctrl -r "iCant" -e 1,500,200,620,450
